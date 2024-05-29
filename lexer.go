d078236577ecf6c5fe2935c603cf4668b6f15bd1
alehoppai <aleh_belski@outlook.com>
2024-05-29 19:21:01 +0200

    reworking and using lexer (not working properly)

--- lexer.go
+++ lexer.go
@@ -1,58 +1,81 @@
 package main
 
 import (
-    "regexp"
-    "strings"
+	"fmt"
+	"log"
+	"regexp"
+	"strings"
 )
 
 func getKeywords() []string {
-    varKeywords := []string{"num", "str", "bool", "opt"}
-    baseKeywords := []string{"struct", "proto", "pub", "mut", "fn", "mod", "return"}
+	varKeywords := []string{"num", "str", "bool", "opt"}
+	baseKeywords := []string{"struct", "proto", "pub", "mut", "fn", "mod", "return"}
 
-    return PrependArray(varKeywords, baseKeywords)
+	return PrependArray(varKeywords, baseKeywords)
 }
 
 func getTokenTypes() map[string]*regexp.Regexp {
-    keywords := getKeywords()
-    tokenTypes := make(map[string]*regexp.Regexp)
+	keywords := getKeywords()
+	tokenTypes := make(map[string]*regexp.Regexp)
 
-    keywordPattern := `\b(` + strings.Join(keywords, "|") + `)\b`
-    tokenTypes["keyword"] = regexp.MustCompile(keywordPattern)
-    tokenTypes["literal"] = regexp.MustCompile(`\b\d+(\.\d+)?b?\b`)
-    tokenTypes["literalString"] = regexp.MustCompile(`"([^"\\]|\\.)*"`)
-    tokenTypes["boolean"] = regexp.MustCompile(`\b(true|false)\b`)
-    tokenTypes["identifier"] = regexp.MustCompile(`&?[a-zA-Z_][a-zA-Z0-9_]*`)
-    tokenTypes["operator"] = regexp.MustCompile(`[+\-*/=]=?`)
-    tokenTypes["delimiter"] = regexp.MustCompile(`[(){};,:.]`)
-    tokenTypes["whitespace"] = regexp.MustCompile(`\s+`)
-    tokenTypes["comment"] = regexp.MustCompile(`#.*`)
+	keywordPattern := `\b(` + strings.Join(keywords, "|") + `)\b`
+	tokenTypes["keyword"] = regexp.MustCompile(keywordPattern)
+	tokenTypes["literal"] = regexp.MustCompile(`\b\d+(\.\d+)?b?\b`)
+	tokenTypes["literalString"] = regexp.MustCompile(`"([^"\\]|\\.)*"`)
+	tokenTypes["boolean"] = regexp.MustCompile(`\b(true|false)\b`)
+	tokenTypes["identifier"] = regexp.MustCompile(`&?[a-zA-Z_][a-zA-Z0-9_]*`)
+	tokenTypes["operator"] = regexp.MustCompile(`[+\-*/=]=?`)
+	tokenTypes["delimiter"] = regexp.MustCompile(`[(){};,:.]`)
+	tokenTypes["whitespace"] = regexp.MustCompile(`\s+`)
+	tokenTypes["comment"] = regexp.MustCompile(`#.*`)
 
-    return tokenTypes
+	return tokenTypes
 }
 
 type Token struct {
-    Type string
-    Value string
+	Type  string
+	Value string
 }
 
 func Lexer(sourceCode string) []Token {
-    var tokens []Token
-    position := 0
-    tokenTypes := getTokenTypes()
-    
+	var tokens []Token
+	position := 0
+	tokenTypes := getTokenTypes()
 
-    for position < len(sourceCode) {
-        var bufToken *Token
+	for position < len(sourceCode) {
+		var bufToken *Token
 
-        for tokeType, regex := range tokenTypes {
-            substring := sourceCode[:position]
+		for tokenType, regex := range tokenTypes {
+			substring := sourceCode[:position]
+			match := regex.FindStringSubmatch(substring)
 
-            if regex.MatchString(substring) {
-                bufToken = &Token{Type: tokeType, Value: }
-            }
-        } 
-    }
+			fmt.Println(bufToken)
+			fmt.Println(tokenType)
+			fmt.Println(position)
+			fmt.Println(match)
+			fmt.Println("========")
 
-    return tokens
+			if len(match) > 0 {
+				bufToken = &Token{Type: tokenType, Value: match[0]}
+			}
+			//if regex.MatchString(substring) {
+			//    bufToken = &Token{Type: tokeType, Value: }
+			//}
+		}
+
+		if bufToken != nil {
+			if bufToken.Type != "comment" && bufToken.Type != "whitespace" {
+				token := Token{Type: bufToken.Type, Value: bufToken.Value}
+				tokens = append(tokens, token)
+				bufToken = nil
+			}
+			position += len(bufToken.Value)
+		} else {
+			log.Fatal("Not a single substring matched token")
+			position += 1
+			continue
+		}
+	}
+
+	return tokens
 }
-
--- main.go
+++ main.go
@@ -5,37 +5,38 @@ import (
 	"io/fs"
 	"log"
 	"os"
-    "path/filepath"
+	"path/filepath"
 )
 
 func readProject(path string) (entries []fs.DirEntry) {
-    entries, err := os.ReadDir(path)
-    if err != nil {
-        log.Fatal(err)
-        return
-    }
+	entries, err := os.ReadDir(path)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
 
-    return
+	return
 }
 
-func main () {
+func main() {
 	fmt.Println("***FIX language compiler***")
 
-    projectPath := os.Args[1]
-    fmt.Println(projectPath)
-    files := readProject(projectPath)
+	projectPath := os.Args[1]
+	fmt.Println(projectPath)
+	files := readProject(projectPath)
 
-    for _, file := range files {
-        if file.IsDir() {
-            continue
-        }
+	for _, file := range files {
+		if file.IsDir() {
+			continue
+		}
 
-        filePath := filepath.Join(projectPath, file.Name())
-        content, err := os.ReadFile(filePath)
-        if err != nil {
-            log.Fatal(err)
-        }
+		filePath := filepath.Join(projectPath, file.Name())
+		content, err := os.ReadFile(filePath)
+		if err != nil {
+			log.Fatal(err)
+		}
 
-        fmt.Println(string(content))
-    }
+		tokens := Lexer(string(content))
+		fmt.Println(tokens)
+	}
 }
